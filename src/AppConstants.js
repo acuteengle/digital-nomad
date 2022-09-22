f67989c072d37015c773c07949544f57f843712d
acuteengle <bce25@cornell.edu>
2022-09-22 16:56:38 +0200

    some of the locations

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -3,172 +3,314 @@ export const LOCATIONS = [
   {
     city: "Istanbul",
     country: "Türkiye",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-09-04",
+    departureDay: "2022-09-18",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "Budapest",
     country: "Hungary",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-08-21",
+    departureDay: "2022-09-04",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "Amsterdam",
     country: "Netherlands",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-08-07",
+    departureDay: "2022-08-21",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "Lisbon",
     country: "Portugal",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-07-21",
+    departureDay: "2022-08-07",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "Hawaii",
     country: "USA",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-07-07",
+    departureDay: "2022-07-17",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "DMV (DC, Maryland, Virginia)",
     country: "USA",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-06-15",
+    departureDay: "2022-07-06",
     video: "",
-    map: "",
+    map: null,
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: null,
+    spending: null,
   },
   {
     city: "Tamarindo",
     country: "Costa Rica",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-05-22",
+    departureDay: "2022-06-15",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "Playa del Carmen",
     country: "Mexico",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-04-20",
+    departureDay: "2022-05-22",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "Medellin",
     country: "Colombia",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-03-13",
+    departureDay: "2022-04-10",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "New Orleans, Louisiana",
     country: "USA",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-02-20",
+    departureDay: "2022-03-13",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "Atlanca, Georgia",
     country: "USA",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-01-30",
+    departureDay: "2022-02-20",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "San Juan, Puerto Rico",
     country: "USA",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2022-01-09",
+    departureDay: "2022-01-30",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 0,
+    spending: 0,
   },
   {
     city: "Barcelona",
     country: "Spain",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2021-10-23",
+    departureDay: "2021-10-29",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 928,
+    spending: 1469,
   },
   {
     city: "Chicago, Illinois",
     country: "USA",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2021-09-08",
+    departureDay: "2021-10-15",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 1587,
+    spending: 1351,
   },
   {
     city: "Bay Area, California",
     country: "USA",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2021-08-20",
+    departureDay: "2021-09-07",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 1260,
+    spending: 2533,
   },
   {
     city: "Seattle, Washington",
     country: "USA",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2021-08-01",
+    departureDay: "2021-08-19",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 1631,
+    spending: 1589,
   },
   {
     city: "Mexico City",
     country: "Mexico",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2021-07-17",
+    departureDay: "2021-07-31",
     video: "",
     map: "",
     blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    personalInstagramPost: "", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 1635,
+    spending: 1890,
+  },
+  {
+    city: "Ithaca, New York",
+    country: "USA",
+    attitude: 0,
+    longitude: 0,
+    arrivalDay: "2021-07-10",
+    departureDay: "2021-07-16-",
+    video: null,
+    map: null,
+    blogPost: "",
+    personalInstagramPost: null, // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 1028,
+    spending: 948,
   },
   {
     city: "New York City, New York",
     country: "USA",
-    video: "",
-    map: "",
-    blogPost: "",
-    housing: 1131,
-    spending: 1387,
+    attitude: 30.26830825183726,
+    longitude: -97.74928650508556,
+    arrivalDay: "2021-06-05",
+    departureDay: "2021-07-09",
+    video: "https://www.youtube.com/watch?v=R2yxMWeNb4g",
+    map: "https://www.google.com/maps/d/edit?mid=1HTSVKACsjdCxswCbMj1Lz_q73zDxPKjK&usp=sharing",
+    blogPost: "https://thequietchew.wordpress.com/2021/07/08/concrete-jungle/",
+    personalInstagramPost: "https://www.instagram.com/p/CQtcYTajoVG/", // acuteengle
+    photosPost: null, // b.eng photography
+    dancePost: null, // instagram reel
+    housing: 1187,
+    spending: 1902,
   },
   {
     city: "Austin, Texas",
     country: "USA",
     lattitude: 30.26830825183726,
     longitude: -97.74928650508556,
-    arrivalDay: '2021-03-28',
-    departureDay: '2021-05-01',
+    arrivalDay: "2021-03-28",
+    departureDay: "2021-05-01",
     video: "https://www.youtube.com/watch?v=K4k1gXZkJiU",
     map: "https://www.google.com/maps/d/edit?mid=178GA8x6t2ypF9qmBf1VK2rEpXGVq5F_F&usp=sharing",
     blogPost: "https://thequietchew.wordpress.com/2021/07/08/good-morning-austin/",
--- src/views/Locations/Locations.js
+++ src/views/Locations/Locations.js
@@ -8,24 +8,22 @@ import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import { LOCATIONS } from "AppConstants";
 
-export const Locations = () => {
-  return (
-    <Container className="pageContainer">
-      <h1 className="pageTitle">Locations</h1>
-      <p>(most recent at the top)</p>
-      <Timeline position="alternate">
-        {LOCATIONS.map(({ city, country, arrivalDay, departureDay }, index) => (
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot />
-              {index !== LOCATIONS.length - 1 && <TimelineConnector />}
-            </TimelineSeparator>
-            <TimelineContent>
-              <p>{`${city}, ${country}`}</p>
-            </TimelineContent>
-          </TimelineItem>
-        ))}
-      </Timeline>
-    </Container>
-  )
-};
+export const Locations = () => (
+  <Container className="pageContainer">
+    <h1 className="pageTitle">Locations</h1>
+    <p>(most recent at the top)</p>
+    <Timeline position="alternate">
+      {LOCATIONS.map(({ city, country, arrivalDay, departureDay }, index) => (
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot />
+            {index !== LOCATIONS.length - 1 && <TimelineConnector />}
+          </TimelineSeparator>
+          <TimelineContent>
+            <p>{`${city}, ${country}`}</p>
+          </TimelineContent>
+        </TimelineItem>
+      ))}
+    </Timeline>
+  </Container>
+);
