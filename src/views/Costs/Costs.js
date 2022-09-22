9f545201fe06c039a0c0e7bcdb850ff6fc2923b9
acuteengle <bce25@cornell.edu>
2022-09-22 22:35:13 +0200

    chart

--- src/views/Costs/Costs.js
+++ src/views/Costs/Costs.js
@@ -1,10 +1,81 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  defaults,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { LOCATIONS } from "AppConstants";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export const Costs = () => {
+  defaults.font.family = "Helvetica Neue";
+
+  const sortedLocations = [...LOCATIONS].filter(location => location.housing !== null && location.spending !== null);
+  sortedLocations.sort((a, b) => a.housing + a.spending - b.housing - b.spending);
+
+  const labels = [];
+  const housingData = [];
+  const spendingData = [];
+  for (const { city, housing, spending } of sortedLocations) {
+    labels.push(city);
+    housingData.push(housing);
+    spendingData.push(spending);
+  }
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "housing",
+        data: housingData,
+        backgroundColor: "#222222",
+      },
+      {
+        label: "spending",
+        data: spendingData,
+        backgroundColor: "#444444",
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    interaction: {
+      mode: "index"
+    },
+    scales: {
+      x: {
+        stacked: true,
+        grid: {
+          display: false
+        }
+      },
+      y: {
+        stacked: true,
+      },
+    },
+  };
+
   return (
-    <Container className='pageContainer'>
+    <Container className="pageContainer">
       <h1>Costs</h1>
+      <p>"monthly"</p>
+      <Bar options={options} data={data} />
     </Container>
   )
 };
