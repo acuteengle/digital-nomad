b573b8abed2dee5f4d8e3f2786744eb4336b5f93
acuteengle <bce25@cornell.edu>
2022-12-18 12:29:49 +0700

    VisLeafletMap

--- src/views/Map/Map.js
+++ src/views/Map/Map.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { Map as PigeonMap, Marker, ZoomControl } from "pigeon-maps";
-import { stamenTerrain } from 'pigeon-maps/providers';
+import { VisLeafletMap } from '@unovis/react';
+import { LeafletMap } from '@unovis/ts';
 import { LOCATIONS } from "AppConstants";
+import './styles.css';
 
 export const Map = () => {
   const navigate = useNavigate();
   const [selectedLocation, setSelectedLocation] = useState(null);
+
+  const pointLatitude = useCallback((d) => d.lattitude, []);
+  const pointLongitude = useCallback((d) => d.longitude, []);
+  const pointBottomLabel = useCallback((d) => d.city, []);
+
   return (
     <Container className="pageContainer">
       <h1>Map</h1>
@@ -17,12 +23,28 @@ export const Map = () => {
           : `"click on a marker"`
         }
       </p>
-      <PigeonMap height={700} provider={stamenTerrain} defaultCenter={[35.977844, -7.986475]} defaultZoom={2}>
-        <ZoomControl />
-        {LOCATIONS.map((location) => (
-          <Marker width={50} anchor={[location.lattitude, location.longitude]} onClick={() => setSelectedLocation(location)} />
-        ))}
-      </PigeonMap>
+      <VisLeafletMap
+        className='mapStyles'
+        height="70vh"
+        data={LOCATIONS}
+        style={`https://api.maptiler.com/maps/positron/style.json?key=4cEIJ9kkNMOX9aeSnXHe`}
+        pointLatitude={pointLatitude}
+        pointLongitude={pointLongitude}
+        pointBottomLabel={pointBottomLabel}
+        clusterRadius={10}
+        pointRadius={6}
+        pointColor='black'
+        clusterExpandOnClick={false}
+        attribution={[
+          `<a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler</a>`,
+          `<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>`
+        ]}
+        events={{
+          [LeafletMap.selectors.point]: {
+            click: (d => setSelectedLocation(d.properties))
+          }
+        }}
+      />
     </Container >
   )
 }
