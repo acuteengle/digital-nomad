73bfc3ff3371c46fff327c6df899cf10bcad7ca1
acuteengle <bce25@cornell.edu>
2022-12-18 12:39:56 +0700

    leaflet doesn't render in netlify

--- src/views/Map/Map.js
+++ src/views/Map/Map.js
@@ -1,18 +1,13 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { VisLeafletMap } from '@unovis/react';
-import { LeafletMap } from '@unovis/ts';
+import { Map as PigeonMap, Marker, ZoomControl } from "pigeon-maps";
+import { stamenTerrain } from 'pigeon-maps/providers';
 import { LOCATIONS } from "AppConstants";
-import './styles.css';
 
 export const Map = () => {
   const navigate = useNavigate();
   const [selectedLocation, setSelectedLocation] = useState(null);
-
-  const pointLatitude = useCallback((d) => d.lattitude, []);
-  const pointLongitude = useCallback((d) => d.longitude, []);
-  const pointBottomLabel = useCallback((d) => d.city, []);
 
   return (
     <Container className="pageContainer">
@@ -23,31 +18,12 @@ export const Map = () => {
           : `"click on a marker"`
         }
       </p>
-      <VisLeafletMap
-        className='mapStyles'
-        height="70vh"
-        data={LOCATIONS}
-        style={`https://api.maptiler.com/maps/positron/style.json?key=4cEIJ9kkNMOX9aeSnXHe`}
-        pointLatitude={pointLatitude}
-        pointLongitude={pointLongitude}
-        pointBottomLabel={pointBottomLabel}
-        clusterRadius={10}
-        pointRadius={6}
-        pointColor='black'
-        clusterExpandOnClick={false}
-        attribution={[
-          `<a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler</a>`,
-          `<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>`
-        ]}
-        events={{
-          [LeafletMap.selectors.point]: {
-            click: (d => {
-              if (d.properties.cluster) return;
-              setSelectedLocation(d.properties);
-            })
-          }
-        }}
-      />
+      <PigeonMap height={700} provider={stamenTerrain} defaultCenter={[35.977844, -7.986475]} defaultZoom={2}>
+        <ZoomControl />
+        {LOCATIONS.map((location) => (
+          <Marker width={50} anchor={[location.lattitude, location.longitude]} onClick={() => setSelectedLocation(location)} />
+        ))}
+      </PigeonMap>
     </Container >
   )
 }
